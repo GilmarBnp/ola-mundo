39742cdbf7317c126c2d18e4e98563eea2f44fa3
GilmarBnp <81029550+GilmarBnp@users.noreply.github.com>
2021-05-12 20:25:06 -0300

    Add files via upload

---
+++ programa funcionário desconto inss irrf/Salario.java
@@ -0,0 +1,15 @@
+package desafioFinal;
+
+public class Salario {
+private Integer salario;
+
+public Integer getSalario() {
+	return salario;
+}
+
+public void setSalario(Integer salario) {
+	this.salario = salario;
+}
+
+
+}
